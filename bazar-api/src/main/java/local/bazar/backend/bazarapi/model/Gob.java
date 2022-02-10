f3d7d13d45db5e98eb311ba387df19292973a5bf
Anar Bakirov <anarbakirov@gmail.com>
2022-02-10 15:07:15 +0400

    devtool unique edited

--- bazar-api/src/main/java/local/bazar/backend/bazarapi/model/Gob.java
+++ bazar-api/src/main/java/local/bazar/backend/bazarapi/model/Gob.java
@@ -1,6 +1,7 @@
 package local.bazar.backend.bazarapi.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -19,7 +20,9 @@ public class Gob implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
     private Long id;
+    private int uid;
 
+    @DateTimeFormat(pattern="dd-MM-yyyy HH:MM:ss")
     private Date gob_date;
     private int status;
     private Date date;
